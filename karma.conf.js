da19b8a4440a9d653b20316c2c044398c81784a2
wengellen <wengellen@gmail.com>
2015-11-29 19:10:06 -0800

    Add Karma test coverage

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+/**
+ * Created by mbp on 11/29/15.
+ */
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'js/*.js',
+            'spec/feedreader.js'
+        ],
+        browsers: ['Chrome'],
+        singleRun: true,
+        reporters: ['progress', 'coverage'],
+        preprocessors: {
+            'js/*.js': ['coverage']
+        },
+        coverageReporter: {
+            // specify a common output directory
+            dir: 'build/reports/coverage',
+            reporters: [
+                // reporters not supporting the 'file' property
+                { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' },
+                // reporters supporting the 'file' property, use 'subdir' to directly
+                // output them in the 'dir' directory
+                { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+                { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+                { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+                { type: 'text', subdir: '.', file: 'text.txt' },
+                { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+            ]
+        }
+    });
+};
